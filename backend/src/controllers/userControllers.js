fe5cb1e3ceacf53c44574bef264afd0829f5e232
MaLuongKhanh <146851832+MaLuongKhanh@users.noreply.github.com>
2025-03-09 11:14:07 +0700

    BE: add api-gateway and auth (authentication)

--- backend/src/controllers/userControllers.js
+++
@@ -1,7 +0,0 @@
-exports.getUsers = (req, res) => {
-    res.json({ message: "Danh sách người dùng" });
-};
-
-exports.createUser = (req, res) => {
-    res.json({ message: "Người dùng đã được tạo" });
-};
